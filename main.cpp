ff305f1c4adbf2394740b996f4e38638145d6185
Sospitas <ptcchrisnis@googlemail.com>
2014-10-16 00:06:43 +0100

    Got Window Rendering

--- main.cpp
+++ main.cpp
@@ -1,6 +1,127 @@
 #include <Windows.h>
+#include <windowsx.h>
+#include <d3d9.h>
 
+#pragma comment (lib, "d3d9.lib");
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+// Global Declarations
+LPDIRECT3D9 d3d; // Pointer to d3d interface
+LPDIRECT3DDEVICE9 d3ddev; // Pointer to device class
+
+void initD3D(HWND hWnd);
+void render_frame(void);
+void cleanD3D(void);
+
+// WindowProc Function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+void initD3D(HWND hWnd)
+{
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);
+
+	D3DPRESENT_PARAMETERS d3dpp;
+
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.hDeviceWindow = hWnd;
+	d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
+	d3dpp.BackBufferWidth = SCREEN_WIDTH;
+	d3dpp.BackBufferHeight = SCREEN_HEIGHT;
+
+	d3d->CreateDevice(D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_HAL,
+		hWnd,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+		&d3dpp,
+		&d3ddev);
+}
+
+void render_frame(void)
+{
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
+
+	d3ddev->BeginScene();
+	// do 3D rendering on the back buffer here
+	d3ddev->EndScene();
+	d3ddev->Present(NULL, NULL, NULL, NULL);
+}
+
+void cleanD3D(void)
+{
+	d3ddev->Release();
+	d3d->Release();
+}
+
+// Entry point of the program
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	return 0;
+	HWND hWnd;
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	//wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = L"WindowClass1";
+
+	RegisterClassEx(&wc);
+
+	hWnd = CreateWindowEx(NULL,
+		L"WindowClass1",
+		L"Sosp Particle System",
+		WS_OVERLAPPEDWINDOW,
+		100,
+		100,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	initD3D(hWnd);
+
+	MSG msg;
+	while (TRUE)
+	{
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT)
+			break;
+
+		// GAME CODE BELOW HERE
+		render_frame();
+	}
+
+	cleanD3D();
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_DESTROY:
+			{
+				PostQuitMessage(0);
+				return 0;
+			}
+			break;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
